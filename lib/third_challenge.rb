e84c6ba115eedb66e4826347eddae71e8f667e53
Anika Christopher <anika.mchristopher@gmail.com>
2019-09-29 20:50:52 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,9 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
-        
       :matriarch => {name: "Lady Montague", age: "54"},
-        
       :hero => {name: "Romeo", age: "15"},
       :hero_friends => [name: "Benovolio", age: "17", name: "Mercutio", age: "18"]
    }, 
