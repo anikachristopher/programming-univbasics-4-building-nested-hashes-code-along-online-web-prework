c2e9ae7768d85d0350c2f8298211986a4db77f4e
Anika Christopher <anika.mchristopher@gmail.com>
2019-09-29 20:37:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,8 +4,7 @@ def third_challenge
       :patriarch => {name: "Lord Montague", age: "53"}
         
       :matriarch => {
-        #your key/value pairs here
-        },
+        
       :hero => {
         #your key/value pairs here
         },
